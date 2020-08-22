a343511ec1dd4f681b0bc1ebcafbbacb20bc5a03
jinyaoMa <jinyao.ma@outlook.com>
2020-08-21 23:03:44 -0400

    initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-page'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
